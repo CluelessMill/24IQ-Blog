59ad28a9653d6e993b389a898c50c1a8c54e0af1
Routybor <routybor@gmail.com>
2024-05-05 13:45:35 +0700

    delete old backend and frontend

--- backend/api/utils/roles_utils.py
+++
@@ -1,5 +0,0 @@
-from ..models import User
-
-
-def admin_check(user: User) -> bool:
-    return user.role == "admin"
--- backend/backend/wsgi.py
+++
@@ -1,7 +0,0 @@
-from os import environ
-
-from django.core.wsgi import get_wsgi_application
-
-environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="backend.settings")
-
-application = get_wsgi_application()
