17575b35d277b792145995d8ee2e582bcdd3090a
Routybor <routybor@gmail.com>
2024-05-10 22:47:21 +0700

    old code delete

--- backend/api/utils/roles_utils.py
+++
@@ -1,8 +0,0 @@
-from ..models import User
-from ..utils.token_utils import AccessToken
-from ..utils.user_utils import authenticate_user
-
-
-def admin_check(token: AccessToken) -> bool | int:
-    # ! No need now, replaced by token.check() returned value to optimize process
-    pass
--- backend/backend/asgi.py
+++
@@ -1,7 +0,0 @@
-from os import environ
-
-from django.core.asgi import get_asgi_application
-
-environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="backend.settings")
-
-application = get_asgi_application()
